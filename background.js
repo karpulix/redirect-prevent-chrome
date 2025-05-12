d4c1bc8e71dc5a2f3065737e68499f329b760eab
Alexander Karpov <karpulix@gmail.com>
2025-05-12 20:34:23 +0400

    fix manifest file

--- background.js
+++ background.js
@@ -1,4 +1,32 @@
 // Background script
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Redirect Prevent extension installed');
+});
+
+// Inject code when page is loaded
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url && tab.url.startsWith('http')) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: () => {
+                window.onbeforeunload = function() {
+                    return 'Redirect detected. Leave page?';
+                };
+            }
+        });
+    }
+});
+
+// Also allow manual injection by clicking the extension icon
+chrome.action.onClicked.addListener((tab) => {
+    if (tab.url && tab.url.startsWith('http')) {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: () => {
+                window.onbeforeunload = function() {
+                    return 'Redirect detected. Leave page?';
+                };
+            }
+        });
+    }
 }); 
--- content.js
+++ content.js
@@ -1,4 +1,17 @@
-// Inject the redirect prevention code
+// Prevent redirects
 window.onbeforeunload = function() {
     return 'Redirect detected. Leave page?';
-}; 
+};
+
+// Also prevent programmatic redirects
+const originalLocation = window.location;
+Object.defineProperty(window, 'location', {
+    get: function() {
+        return originalLocation;
+    },
+    set: function(url) {
+        if (confirm('Redirect detected. Leave page?')) {
+            originalLocation.href = url;
+        }
+    }
+}); 
